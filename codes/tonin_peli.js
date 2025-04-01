4a3bf1e35a62de2960002d74f0204af2dcc1afe7
tonipekkala24 <tpekkala24@students.oamk.fi>
2025-04-01 18:08:10 +0300

    shuffle and flipCard function added

--- codes/tonin_peli.js
+++ codes/tonin_peli.js
@@ -3,16 +3,18 @@ const startScreen = document.querySelector("#start-game-screen");
 const playerScore = document.querySelector(".score");
 const playButton = document.querySelector(".play-button");
 const cards = [];
+let firstCard, secondCard;
 let score = 0;
+document.querySelector(".score").textContent = score;
 
-createCards()
-generateCards()
+createCards();
+shuffleCards();
+generateCards();
 
 playButton.addEventListener("click", () => {
     startScreen.style.display = "none";
     gameContainer.style.display = "grid";
-
-})
+});
 
 function createCards() {
     // Makes copy of each card once.
@@ -29,15 +31,36 @@ function createCards() {
         };        
     };
 };
+
+// Shuffles cards array.
+function shuffleCards() {
+    cards.sort(() => Math.random() - 0.5);
+};
+
 // Creates card images, card elements and adds them to HTML.
 function generateCards() {
     for (let card of cards) {
-        const cardImage = document.createElement("img")
+        const cardImage = document.createElement("img");
         const cardElement = document.createElement("div");
 
-        cardImage.src = card.frontimg
+        cardImage.src = card.backimg;
+        cardElement.setAttribute("data-name", card.id)
         cardElement.classList.add("card");
-        cardElement.appendChild(cardImage)
-        gameContainer.appendChild(cardElement)
-    }
-}
+        cardElement.appendChild(cardImage);
+        gameContainer.appendChild(cardElement);
+        // Adds click event to the card element so it flips when clicked.
+        cardElement.addEventListener("click", flipCard);
+    };
+};
+
+
+function flipCard(e) {
+    const cardElements = gameContainer.getElementsByTagName("div")
+    console.log(this);
+    // When clicking a card, this checks if div's data-name is same as cards id.
+    // If it is, then flip the card.
+    for (let i = 0; i < cards.length; i++) {
+        if (this.dataset.name == cards[i].id)
+            e.target.src = cards[i].frontimg;
+    };
+};
