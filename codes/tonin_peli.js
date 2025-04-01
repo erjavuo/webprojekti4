2ccdb72c2ad8d33546634aa9b39119729486754d
tonipekkala24 <tpekkala24@students.oamk.fi>
2025-04-01 12:00:34 +0300

    added function that creates and adds card objects to the images array

--- codes/tonin_peli.js
+++ codes/tonin_peli.js
@@ -1,1 +1,18 @@
 const images = [];
+
+
+function createCards() {
+    // Makes copy of each card once.
+    for(let i = 0; i < 2; i++) {
+        // Creates and adds card objects to the array.
+        for(let i = 1; i < 5; i++) {
+            const card = {
+                id: i,
+                frontimg: `../images/tonin_muistipeli/kuva${i}.jpg`,
+                backimage: "../images/tonin_muistipeli/backimage.jpg",
+                flipped: false,
+            };
+            images.push(card);
+        };        
+    };
+};
