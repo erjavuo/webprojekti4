e018163f479e6b9d5c27e9b086d0a2acc4971c0b
tonipekkala24 <tpekkala24@students.oamk.fi>
2025-04-01 22:50:12 +0300

    added checkIfMatching, disableCards, unflipCards and resetCards functions

--- codes/tonin_peli.js
+++ codes/tonin_peli.js
@@ -4,8 +4,12 @@ const playerScore = document.querySelector(".score");
 const playButton = document.querySelector(".play-button");
 const cards = [];
 let firstCard, secondCard;
+let lockTheGame = false;
 let score = 0;
 document.querySelector(".score").textContent = score;
+
+const divs = gameContainer.getElementsByTagName("div");
+const cardElements = [];
 
 createCards();
 shuffleCards();
@@ -25,14 +29,13 @@ function createCards() {
                 id: i,
                 frontimg: `../images/tonin_muistipeli/kuva${i}.jpg`,
                 backimg: "../images/tonin_muistipeli/backimage.jpg",
-                flipped: false,
             };
             cards.push(card);
         };        
     };
 };
 
-// Shuffles cards array.
+// Shuffles the cards array.
 function shuffleCards() {
     cards.sort(() => Math.random() - 0.5);
 };
@@ -51,16 +54,62 @@ function generateCards() {
         // Adds click event to the card element so it flips when clicked.
         cardElement.addEventListener("click", flipCard);
     };
+
+    for (let i = 0; i < divs.length; i++) {
+        cardElements.push(divs[i]);
+    };
 };
 
 
 function flipCard(e) {
-    const cardElements = gameContainer.getElementsByTagName("div")
-    console.log(this);
-    // When clicking a card, this checks if div's data-name is same as cards id.
-    // If it is, then flip the card.
-    for (let i = 0; i < cards.length; i++) {
-        if (this.dataset.name == cards[i].id)
-            e.target.src = cards[i].frontimg;
+    // If lockTheGame is true, the game does not let player to flip the cards.
+    if (lockTheGame) return;
+    // The game wont let player to flip same card twice.
+    if (this === firstCard) return;
+    e.target.src = cards[cardElements.indexOf(this)].frontimg;
+    if (!firstCard) {
+        firstCard = this;
+        console.log(firstCard);
+        return
+    }
+
+    secondCard = this;
+    lockTheGame = true;
+    checkIfMatching();
+};
+
+function checkIfMatching() {
+    const isMatch = firstCard.dataset.name === secondCard.dataset.name;
+    if (isMatch) {
+        score+=2
+        document.querySelector(".score").textContent = score;
+        disableCards();
+    } else {
+        unflipCards();
     };
+};
+
+// When the player gets same pair of cards, the function disables the click event of the cards.
+function disableCards() {
+    firstCard.removeEventListener("click", flipCard)
+    secondCard.removeEventListener("click", flipCard)
+    resetCards();
+};
+
+// After 2 seconds when the cards didnt match, the function flips the cards back.
+function unflipCards() {
+    const firstCardimg = firstCard.getElementsByTagName("img")
+    const secondCardimg = secondCard.getElementsByTagName("img")
+
+    setTimeout(() => {
+        firstCardimg[0].src = cards[cardElements.indexOf(firstCard)].backimg;
+        secondCardimg[0].src = cards[cardElements.indexOf(secondCard)].backimg;
+        resetCards()
+    }, 2000);
+};
+
+function resetCards() {
+    firstCard = null;
+    secondCard = null;
+    lockTheGame = false;
 };
