59ec12ffcc7eacb8e31d3ca94cc746f4ac10b879
Rasse23 <rpekkane24@students.oamk.fi>
2025-04-07 11:42:49 +0300

    added Continuetonextcategory function, cleaned some code little bit

--- codes/rasmuksenpeli.js
+++ codes/rasmuksenpeli.js
@@ -5,33 +5,60 @@ let i = 1;
 let index = 0;
 
 
+let pic1 = document.getElementById('pic1');
+let images = document.getElementsByTagName('img');
+let img1 = document.getElementById('img1');
+let img2 = document.getElementById('img2');
+
+pic1.addEventListener('click', setrandomImageBirds);
+
+
+function ContinuetoNextCategory(){
+
+
+    if(index <= 2){
+
+        document.getElementById("question").innerHTML = categories[index];
+
+        index++;
+
+
+    }
+
+    else{
+
+        index = 0;
+
+    }
+
+
+    }
 
 
 
-setrandomImageBirds();
-setCategories();
+
+
+
 // hakee kuvat listasta ja asettaa satunnaisen kuvan lintu kategorialle, sisältäen myös vääriä vastauksia muista kategorioista
 function setrandomImageBirds(){
 
 
-    var images = document.getElementsByTagName('img');
-    var listbirds=["../images/rasmuksenpelin_kuvat/Birds/0b.jpg","../images/rasmuksenpelin_kuvat/Birds/0a.jpg", "../images/rasmuksenpelin_kuvat/Birds/0c.jpg", "../images/rasmuksenpelin_kuvat/Birds/0d.jpg", "../images/rasmuksenpelin_kuvat/forest animals/bear-5120104_1280.jpg", "../images/rasmuksenpelin_kuvat/Birds/0b.jpg" ];
-    var randomNumberbirds = Math.ceil(Math.random() * 5);
-    let img1 = document.getElementById('img1');
-    img1.addEventListener('click', setrandomImageBirds);
+    var listbirds=["../images/rasmuksenpelin_kuvat/Birds/0b.jpg","../images/rasmuksenpelin_kuvat/Birds/0a.jpg", "../images/rasmuksenpelin_kuvat/Birds/0c.jpg", "../images/rasmuksenpelin_kuvat/Birds/0d.jpg", "../images/rasmuksenpelin_kuvat/Birds/0b.jpg" ];
+    var randomNumberbirds = Math.ceil(Math.random() * 4);
     img1.src=listbirds[randomNumberbirds];
+    
 
-    var listbirds2=["../images/rasmuksenpelin_kuvat/Birds/0b.jpg", "../images/rasmuksenpelin_kuvat/Birds/0d.jpg", "../images/rasmuksenpelin_kuvat/Birds/0a.jpg", "../images/rasmuksenpelin_kuvat/fishes/perch-7521273_1280.jpg"];
+    var listbirds2=["../images/rasmuksenpelin_kuvat/forest animals/fox-4589927_1280.jpg", "../images/rasmuksenpelin_kuvat/fishes/trout.jpg", "../images/rasmuksenpelin_kuvat/forest animals/bear-5120104_1280.jpg", "../images/rasmuksenpelin_kuvat/fishes/perch-7521273_1280.jpg"];
     var randomNumberbirds2 = Math.ceil(Math.random() * 3);
-    let img2 = document.getElementById('img2');
-    img2.addEventListener('click', setrandomImageBirds);
     img2.src=listbirds2[randomNumberbirds2];
-
 
     
 
 }
 
+setCategories();
+
+//Function that will get the h2 element from the html and shows the category according the current number of index element
 function setCategories(){
 
     categoryquestion.textContent = categories[index];
@@ -39,22 +66,12 @@ function setCategories(){
 
 }
 
-function imageClicked(event){
-
-if(event.currentTarget.name == answers[index]){
-
-    console.log('CORRECT ANSWER');
-
-}else{
-
-    console.log('WRONG ANSWER');
-
-}
 
 
 
 
-}
+
+
     
 
     
