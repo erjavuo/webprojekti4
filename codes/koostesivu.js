28ce7e3df057bb7f045c0329d82d202af49316a6
tonipekkala24 <tpekkala24@students.oamk.fi>
2025-04-08 15:47:31 +0300

    memorygame points from session storage now goes to koostesivu.html

--- codes/koostesivu.js
+++ codes/koostesivu.js
@@ -1,1 +1,7 @@
+const memoryGamePointsSpan = document.querySelector("#muistipeli-pisteet");
 const memoryGamePoints = sessionStorage.getItem("memoryGamePoints");
+
+// If points in session storage is not null, adds points from session storage to website 
+if (memoryGamePoints !== null) {
+    memoryGamePointsSpan.textContent = memoryGamePoints
+}
