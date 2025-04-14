040a1f110b137eda6f2d1ebca909791c28d42e62
veetipyoria <vpyoria24@students.oamk.fi>
2025-04-14 21:12:09 +0300

    Fixed the functionality of startButton

--- codes/veetinpeli.js
+++ codes/veetinpeli.js
@@ -4,12 +4,16 @@ const words = [
     "Jääkausi", "Kraateri", "Joki", "Lämpövyöhyke", "Koli", "Manner"
   ];
 
+const button = document.getElementById('startButton');
+
+button.addEventListener("click", startGame);
 
   let currentWord = '';
   let score = 0;
   let timeLeft = 60;
   let timerInterval;
-
+ 
+  // Function that starts the game
   function startGame(){
     document.getElementById("startScreen").style.display = "none";
     document.getElementById("gameScreen").style.display = "container";
